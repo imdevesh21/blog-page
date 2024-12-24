eebe1488339e35bb85be0fbc3521a6dbf0913d04
imdevesh21 <deveshsharma.it23@jecrc.ac.in>
2024-12-24 22:24:29 +0530

    added changes

--- main.js
+++ main.js
@@ -22,13 +22,14 @@ closeNavBtn.addEventListener('click', closeNav);
 const sidebar = document.querySelector('aside');
 const showSidebarBtn = document.querySelector('#show__sidebar-btn');
 const hideSidebarBtn = document.querySelector('#hide__sidebar-btn');
-
+// show sidebar
 const showSidebar = () => {
     sidebar.style.left = '0';
     showSidebarBtn.style.display = 'none';
     hideSidebarBtn.style.display = 'inline-block';
 
 }
+// hide sidebar
 const hideSidebar = () => {
     sidebar.style.left = '-100%';
     showSidebarBtn.style.display = 'inline-block';
